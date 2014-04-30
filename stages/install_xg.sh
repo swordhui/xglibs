61d1a103cb42b0890fb5a1f87258f39da297ae10
szhang <szhang@vetronicsltd.com.cn>
2014-04-30 18:57:57 +0800

    ready for release.

---
+++ stages/install_xg.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# Warning: when switching from a 8bit to a 9bit font,
+# the linux console will reinterpret the bold (1;) to
+# the top 256 glyphs of the 9bit font. This does
+# not affect framebuffer consoles
+NORMAL="\\033[0;39m" # Standard console grey
+SUCCESS="\\033[1;32m" # Success is green
+WARNING="\\033[1;33m" # Warnings are yellow
+FAILURE="\\033[1;31m" # Failures are red
+INFO="\\033[1;36m" # Information is light cyan
+BRACKET="\\033[1;34m" # Brackets are blue
+
+#show info in cyan color, $1 is information.
+showinfo()
+{
+	echo -e  $INFO"$1"$NORMAL
+}
+
+#show OK message in green color, $1 is information.
+showOK()
+{
+	echo -e $SUCCESS"$1"$NORMAL
+}
+
+#show OK message in green color, $1 is information.
+showFailed()
+{
+	echo -e $FAILURE"$1"$NORMAL
+}
+
+#check return code. show $1 if error.
+err_check()
+{
+	if [ $? != 0 ]; then
+		echo -e $FAILURE"$1"
+		exit 1
+	fi
+}
+
+
+#select a device to install system to
+#device name put to $rootdev
+select_device()
+{
+	#dlist=$(fdisk -lu | grep "s@[^/].*@@g")
+	dlist=$(fdisk -lu)
+
+	echo $dlist
+
+
+
+}
+
+select_device
+
+
