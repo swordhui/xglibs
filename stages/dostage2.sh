5d50ffad509b115244e13f8919aa6f7c85ab0ad2
xguser <xguser@xiangelinux.com>
2020-10-13 14:01:17 +0800

    xiange 2020:stage 2 ready

--- stages/dostage2.sh
+++ stages/dostage2.sh
@@ -19,25 +19,18 @@ showinfo "updating gtk icons.."
 gtk-update-icon-cache
 err_check "gtk update icon cache  failed."
 
-#showinfo "updating mime info..."
-#update-desktop-database /usr/share/applications
-#err_check "gtk update icon cache  failed."
+showinfo "updating mime info..."
+update-desktop-database /usr/share/applications
+glib-compile-schemas /usr/share/glib-2.0/schemas
+err_check "gtk update icon cache  failed."
 
-
-#showinfo "enable slim.."
-#systemctl enable slim
-#err_check "enable slim failed."
-
-#showinfo "enable fvwm-xiange"
-#systemctl enable xiange-welcome
-#err_check "enable slim failed."
-
-
-
+showinfo "enable gtkgreet.."
+systemctl enable greetd
+err_check "enable greetd failed."
 
 
 showinfo "creating user xiang..."
-useradd -m -G audio,video,tty,cdrom,camera,gpkg,wheel xiange
+useradd -m -G audio,video,tty,usb,cdrom,camera,gpkg,wheel,net,dialout,systemd-journal,dhcpcd,systemd-resolve xiange
 err_check "add user xiange failed."
 
 showinfo "setting password for xiange..."
